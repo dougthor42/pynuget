5880fd2ef3e22c31179b2fce9286416af751759f
Douglas Thor <doug.thor@gmail.com>
2018-04-20 21:30:06 -0700

    Stubs for routes.py

---
+++ lib/pynuget/routes.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    raise NotImplementedError
+
+def push():
+    raise NotImplementedError
+
+@app.route('/count')
+def count():
+    raise NotImplementedError
+
+@app.route('/delete')
+def delete():
+    raise NotImplementedError
+
+@app.route('/download')
+def download():
+    raise NotImplementedError
+
+@app.route('find_by_id')
+def find_by_id:
+    raise NotImplementedError
+
+@app.route('/search')
+def search():
+    raise NotImplementedError
+
+@app.route('/updates')
+def updates():
+    raise NotImplementedError
