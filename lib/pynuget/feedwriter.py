96dac636b30fe1afc24c1ebcf52da3c69e9cdb3d
Douglas Thor <doug.thor@gmail.com>
2018-04-20 21:25:01 -0700

    Stubs for feedwriter.py

---
+++ lib/pynuget/feedwriter.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+class FeedWriter(object):
+
+    def __init__(self):
+        pass
+
+    def write(self):
+        raise NotImplementedError
+
+    def write_to_output(self):
+        raise NotImplementedError
+
+    def begin_feed(self):
+        raise NotImplementedError
+
+    def add_entry(self):
+        raise NotImplementedError
+
+    def add_entry_meta(self):
+        raise NotImplementedError
+
+    def render_meta_boolean(self):
+        raise NotImplementedError
+
+    def format_date(self):
+        raise NotImplementedError
+
+    def render_dependencies(self):
+        raise NotImplementedError
+
+    def format_target_framework(self):
+        raise NotImplementedError
+
+    def add_with_attributes(self):
+        raise NotImplementedError
+
+    def add_meta(self):
+        raise NotImplementedError
