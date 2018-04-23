5fa77a3fba1c8f1bc79c1d1a4c078f490d73b6ce
Douglas Thor <doug.thor@gmail.com>
2018-04-23 11:26:06 -0700

    Add test stubs for all db functions.

--- tests/test_db.py
+++ tests/test_db.py
@@ -22,7 +22,6 @@ class TestDb(object):
     def test_create_schema(self, session):
         pass
 
-
     def test_count_packages(self, session):
         # insert some dummy data
         pkg = db.Package(title="dummy", latest_version="1.0.0")
@@ -30,3 +29,47 @@ class TestDb(object):
         session.commit()
 
         assert db.count_packages(session) == 1
+
+    def test_search_packages(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.search_packages()
+
+    def test_package_updates(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.package_updates()
+
+    def test_find_by_id(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.find_by_id()
+
+    def test_parse_order_by(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.parse_order_by()
+
+    def test_do_search(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.do_search()
+
+    def test_validate_id_and_version(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.validate_id_and_version()
+
+    def test_increment_download_count(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.increment_download_count()
+
+    def test_insert_or_update_package(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.insert_or_update_package()
+
+    def test_insert_version(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.insert_version()
+
+    def test_delete_version(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.delete_version()
+
+    def test_build_in_clause(self, session):
+        with pytest.raises(NotImplementedError) as e_info:
+            db.build_in_clause()
