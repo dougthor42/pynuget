e377e3fea29a3e3a5579efa1055afe4162cdb09a
Douglas Thor <doug.thor@gmail.com>
2018-04-25 09:43:05 -0700

    Forgot to import routes in __init__...

--- src/pynuget/__init__.py
+++ src/pynuget/__init__.py
@@ -5,3 +5,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# contrary to Python style, the view imports must be *after* the application
+# object is created.
+# http://flask.pocoo.org/docs/0.11/patterns/packages/#simple-packages
+try:
+    import pynuget.routes
+except FileNotFoundError:
+    raise
+    #logger.error("Can't import pynuget.routes.")
