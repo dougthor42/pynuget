035ebff68167add3d3a7a9f3b1a5c32d72115b4c
Douglas Thor <doug.thor@gmail.com>
2018-04-25 13:23:05 -0700

    Fill in test_require_auth, mark the rest as skip

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,16 +5,21 @@
 import pytest
 
 from pynuget import core
+from pynuget import app
 
 
+@pytest.mark.skip("Not Implemented")
 def test_api_error():
     pass
 
 
 def test_require_auth():
-    pass
+    assert app.config['API_KEYS'] == {"ChangeThisKey"}
+    assert core.require_auth('ChangeThisKey')
+    assert not core.require_auth("Invalid Key")
 
 
+@pytest.mark.skip("Not Implemented")
 def test_request_method():
     pass
 
@@ -23,5 +28,6 @@ def test_get_pacakge_path():
     assert core.get_package_path(1, '0.0.1') == './packagefiles/1/0.0.1.nupkg'
 
 
+@pytest.mark.skip("Not Implemented")
 def test_url_scheme():
     pass
