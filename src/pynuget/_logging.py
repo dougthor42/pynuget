767f056f95e8d1d9c4a5e6cbf3536952c85ab457
Douglas Thor <doug.thor@gmail.com>
2018-04-25 14:11:17 -0700

    Initial commit of _logging.py with stubs

---
+++ src/pynuget/_logging.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Logging setup and handlers.
+"""
+
+import logging
+
+
+def setup_logging():
+    pass
+
+
+def _setup_file_logging():
+    pass
+
+
+def _setup_console_logging():
+    pass
