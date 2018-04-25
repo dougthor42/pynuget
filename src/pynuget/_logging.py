088d35a284d5b9391257eac3484d93ec4bba235d
Douglas Thor <doug.thor@gmail.com>
2018-04-25 14:50:57 -0700

    Fill in _setup_file_logging with custom rotating compressing file handler

--- src/pynuget/_logging.py
+++ src/pynuget/_logging.py
@@ -2,14 +2,38 @@
 """
 Logging setup and handlers.
 """
-
+import zlib
 import logging
+import os
+from pathlib import Path
+from logging.handlers import RotatingFileHandler
 
 
 LOG_LEVEL_BASE = logging.DEBUG
 LOG_LEVEL_FILE = LOG_LEVEL_BASE
 LOG_LEVEL_CONSOLE = logging.DEBUG
 LOG_LEVEL_GUI = LOG_LEVEL_BASE
+
+
+# Custom namer and rotator.
+# Taken from https://docs.python.org/3/howto/logging-cookbook.html
+def _gzip_namer(name):
+    return name + ".gz"
+
+
+def _gzip_rotator(source, dest):
+    # Compress the source file into our dest
+    with open(source, 'rb') as open_source:
+        data = open_source.read()
+        compressed = zlib.compress(data)
+        with open(dest, 'wb') as open_dest:
+            open_dest.write(compressed)
+
+    # Keep a copy of the most recent rotation uncompressed to make things
+    # easier for users.
+    os.replace(source, dest + ".1")
+
+    os.remove(source)
 
 
 def setup_logging(to_console=True, to_file=False, log_file=None):
@@ -26,8 +50,18 @@ def setup_logging(to_console=True, to_file=False, log_file=None):
     return logger
 
 
-def _setup_file_logging():
-    pass
+def _setup_file_logging(logger, log_file):
+
+    Path(log_file).touch(mode=0o0664, exist_ok=True)
+
+    handler = RotatingFileHandler(log_file, maxBytes=1e8)
+    handler.rotator = _gzip_rotator
+    handler.namer = _gzip_namer
+    handler.setLevel(LOG_LEVEL_FILE)
+    handler.set_name("File Handler")
+    logger.addHandler(handler)
+
+    logger.info("File logging initialized")
 
 
 def _setup_console_logging(logger):
