6ffde0e1bc2f58a8f1a07e982eaf11aaf7131e41
Douglas Thor <doug.thor@gmail.com>
2018-04-25 14:21:28 -0700

    Fill out setup_logging

--- src/pynuget/_logging.py
+++ src/pynuget/_logging.py
@@ -12,8 +12,18 @@ LOG_LEVEL_CONSOLE = logging.DEBUG
 LOG_LEVEL_GUI = LOG_LEVEL_BASE
 
 
-def setup_logging():
-    pass
+def setup_logging(to_console=True, to_file=False, log_file=None):
+    # Create the logger
+    logger = logging.getLogger()
+    logger.setLevel(LOG_LEVEL_BASE)
+
+    if to_console:
+        _setup_console_logging(logger)
+
+    if to_file:
+        _setup_file_logging(logger, log_file)
+
+    return logger
 
 
 def _setup_file_logging():
