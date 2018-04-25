5dbac6a20a476179e5e98338733afa70208b40a5
Douglas Thor <doug.thor@gmail.com>
2018-04-25 10:24:32 -0700

    Update routes.index

--- src/pynuget/routes.py
+++ src/pynuget/routes.py
@@ -4,6 +4,8 @@
 
 # Third-Party
 from flask import request
+from flask import Response
+import sqlalchemy as sa
 
 from pynuget import app
 
@@ -13,10 +15,15 @@ def root():
     return "Hello World!"
 
 
+@app.route('/', methods=['GET', 'PUT', 'DELETE'])
 @app.route('/index', methods=['GET', 'PUT', 'DELETE'])
 def index():
     if request.method == 'PUT':
         push()
+
+    resp = Response("<?xml version='1.0' encoding='utf-8' standalone='yes'?>")
+    resp.headers['Content-Type'] = 'text/plain; charset=utf-8'
+    return resp
 
 
 def push():
