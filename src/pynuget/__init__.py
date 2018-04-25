aa8cb9cb64a39611dba295014615a0310aa2022b
Douglas Thor <doug.thor@gmail.com>
2018-04-25 13:10:25 -0700

    Import the config file to Flask during __init__

--- src/pynuget/__init__.py
+++ src/pynuget/__init__.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 """
+import os
 
 from flask import Flask
 
+# TODO: switch to instance path, move config to look in /var/www
+#instance_path = "/var/www/pynuget/instance"
+instance_path = os.path.abspath(".")
+config_file = os.path.join(instance_path, "config.py")
+
 app = Flask(__name__)
+app.config.from_pyfile(config_file)
 
 # contrary to Python style, the view imports must be *after* the application
 # object is created.
