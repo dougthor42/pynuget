e448d5fa615b85671492d47e454acf91b98fef2b
Douglas Thor <doug.thor@gmail.com>
2018-04-26 12:43:51 -0700

    Update config.py to include all variables that can be set by cli

--- config.py
+++ config.py
@@ -11,11 +11,23 @@ if sys.platform == 'win32':
     LOG_DIR = 'C:/Logs/pynuget'
 LOG_PATH = os.path.normpath(os.path.join(LOG_DIR, LOG_FILE))
 
+# The database backend to use.
+# Must be ond of ("sqlite", "mysql", "postgresql")
+DB_BACKEND = "sqlite"
+
 # Schema name or path to SQLite file.
-DB_NAME = ""
+DB_NAME = "nuget_packages.sqlite"
+
+# Server path: root path for things like the SQLite file and the packge files.
+# Must be asolute.
+SERVER_PATH = "/var/www/pynuget"
 
 # Where nuget packages will be saved to and sourced from.
-PACKAGE_DIR = "/var/www/pynuget/packagefiles"
+# Can be absolute or relative. Defaults to $SERVER_PATH\$PACAKGE_DIR
+PACKAGE_DIR = "nuget_packages"
+
+# The name of the Apache configuration file
+APACHE_CONFIG = "pynuget.conf"
 
 # A list (well, set) of valid API Keys
 API_KEYS = {
