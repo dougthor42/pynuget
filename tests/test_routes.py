6254ea9e7fcb330f6bd128989bbba52444d29c9a
Douglas Thor <doug.thor@gmail.com>
2018-04-30 14:33:42 -0700

    Add some tests for the 'push' route (ok it's not really a route...)

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,6 +2,8 @@
 """
 """
 import os
+import shutil
+from io import BytesIO
 
 import pytest
 
@@ -15,21 +17,45 @@ DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
 @pytest.fixture
 def client():
-    # override the default paths
+    # override the default variables.
+    # TODO: Move this to just a different config file.
     old_server_path = app.config['SERVER_PATH']
     old_package_dir = app.config['PACKAGE_DIR']
-    app.config['SERVER_PATH'] = DATA_DIR
-    app.config['PACKAGE_DIR'] = '.'
-    commands._create_directories(app.config['SERVER_PATH'],
-                                 app.config['PACKAGE_DIR'])
+    server_path = app.config['SERVER_PATH'] = os.path.join(DATA_DIR, 'server')
+    pkg_dir = app.config['PACKAGE_DIR'] = 'pkgs'
+    old_api_keys = app.config['API_KEYS']
+    api_keys = app.config['API_KEYS'] = ['no_key']
+
+    commands._create_directories(server_path, pkg_dir)
     commands._create_db(app.config['DB_BACKEND'],
                         app.config['DB_NAME'],
-                        app.config['SERVER_PATH'])
+                        server_path)
 
     client = app.test_client()
     yield client
+
+    # Cleanup
+    os.remove(os.path.join(server_path, app.config['DB_NAME']))
+    shutil.rmtree(str(server_path), ignore_errors=False)
     app.config['SERVER_PATH'] = old_server_path
     app.config['PACKAGE_DIR'] = old_package_dir
+    app.config['API_KEYS'] = old_api_keys
+
+
+@pytest.fixture
+def put_header():
+    header = {
+        'X-Nuget-Client-Version': '4.6.2',
+        'X-Nuget-ApiKey': 'no_key',
+        'Connection': 'Keep-Alive',
+        'User-Agent': 'NuGet Command Line/4.6.2 (Microsoft Windows NT 6.2.9200.0)',
+        'Expect': '100-continue',
+        'Accept-Language': 'en-US',
+        'Content-Type': 'multipart/form-data; boundary="0912807c-e081-4c9f-8228-67484e6023a4"',
+        'Host': 'localhost:5000',
+        'Accept-Encoding': 'gzip, deflate',
+    }
+    return header
 
 
 def test_root_get(client):
@@ -50,6 +76,46 @@ def test_index_post(client):
 
 def test_push():
     pass
+
+
+def test_push_no_auth(client, put_header):
+    put_header.pop('X-Nuget-ApiKey')
+    rv = client.put(
+        '/api/v2/package/',
+        headers=put_header,
+        follow_redirects=True,
+    )
+    assert rv.status_code == 401
+
+
+def test_push_invalid_auth(client, put_header):
+    app.config['API_KEYS'] = ""
+    rv = client.put(
+        '/api/v2/package/',
+        headers=put_header,
+        follow_redirects=True,
+    )
+    assert rv.status_code == 401
+
+
+def test_push_no_file(client, put_header):
+    rv = client.put(
+        '/api/v2/package/',
+        headers=put_header,
+        follow_redirects=True,
+    )
+    assert rv.status_code == 409
+
+
+def test_push_invalid_file(client, put_header):
+    data = {'package': (BytesIO(b'Dummy'), 'filename.nupkg')}
+    rv = client.put(
+        '/api/v2/package/',
+        headers=put_header,
+        follow_redirects=True,
+        data=data,
+    )
+    assert rv.status_code == 400
 
 
 def test_count(client):
