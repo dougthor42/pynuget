5228ab797465a107cfb6a562ad838d0c93eb3f9f
Douglas Thor <doug.thor@gmail.com>
2018-04-30 09:28:16 -0700

    Add test__db_data_to_dict

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -9,6 +9,9 @@ import pytest
 
 from pynuget import commands
 from pynuget import app
+from pynuget import db
+
+from .test_db import session
 
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
@@ -40,3 +43,12 @@ def test__get_packages_from_files(package_data, package_dir):
     for key, value in data.items():
         assert key in package_data
         assert set(value) == set(package_data[key])
+
+
+def test__db_data_to_dict(session):
+    expected = {'dummy': ['0.0.1', '0.0.2', '0.0.3']}
+    data = db.search_packages(session, include_prerelease=True)
+    result = commands._db_data_to_dict(data)
+    for key, value in result.items():
+        assert key in expected
+        assert set(value) == set(expected[key])
