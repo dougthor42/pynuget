18119a90d6f53181b2d77cc3fe4a37fb37d97070
Douglas Thor <doug.thor@gmail.com>
2018-05-03 08:25:37 -0700

    Add all NuGetResponse classes.

--- src/pynuget/nuget_response.py
+++ src/pynuget/nuget_response.py
@@ -108,16 +108,302 @@ class SearchResultVersionResponse(NuGetResponse):
         self.downloads = downloads
 
 
-class MetadataResponse(NuGetResponse):
-    pass
+class MetadataIndexResponse(NuGetResponse):
+
+    def __init__(self, count, items):
+        """
+        count : int
+        items : array of RegistrationPage objects
+        """
+        self.count = count
+        self.items = items
+
+
+class MetadataIndexPage(NuGetResponse):
+
+    json_key_map = {
+        "id_": "@id",
+    }
+
+    def __inti__(self, id_, count, lower, upper, items=None, parent=None):
+        """
+        id_ : string
+        count : int
+        lower : string
+        upper : string
+        items : array of MetadataIndexLeaf objects
+        parent : string
+        """
+        self.id_ = id_
+        self.count = count
+        self.lower = lower
+        self.upper = upper
+        self.items = items
+        self.parent = parent
+
+
+class MetadataIndexLeaf(NuGetResponse):
+
+    json_key_map = {
+        "id_": "@id",
+        "catalog_entry": "catalogEntry",
+        "pacakge_content": "pacakgeContent",
+    }
+
+    def __init__(self, id_, catalog_entry, package_content):
+        """
+        id_ : str
+        catalog_entry : CatalogEntry object
+        pacakge_content : str
+        """
+        self.id_ = id_
+        self.catalog_entry = catalog_entry
+        self.pacakge_content = package_content
+
+
+class CatalogEntry(NuGetResponse):
+
+    json_key_map = {
+        "id_": "@id",
+        "pkg_id": "id",
+        "dependency_groups": "dependencyGroups",
+        "icon_url": "iconUrl",
+        "license_url": "licenseUrl",
+        "min_client_version": "minClientVersion",
+        "project_url": "projectUrl",
+        "require_license_acceptance": " requireLicenseAcceptance",
+    }
+
+    def __init__(self, id_, pkg_id, version, **kwargs):
+        """
+        id_ : str
+        pkg_id : str
+        vesion : str
+        authors : str or list of str
+        dependency_groups : array of PackageDependencyGroup objects
+        description : str
+        icon_url : str
+        license_url : str
+        listed : bool
+        min_client_version : str
+        project_url : str
+        published : str (ISO 8601 timestamp)
+        require_license_acceptance : bool
+        summary : str
+        tags : str or list of str
+        title : str
+        """
+        self.id_ = id_
+        self.pkg_id = pkg_id
+        self.version = version
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+class PackageDependencyGroup(NuGetResponse):
+
+    json_key_map = {
+        "target_framework": "targetFramework",
+    }
+
+    def __init__(self, target_framework, dependencies=None):
+        """
+        target_framework : str
+        dependencies : array of PackageDependency objects
+        """
+        self.target_framework = target_framework
+        self.dependencies = dependencies
+
+
+class PackageDependency(NuGetResponse):
+
+    json_key_map = {
+        "id_": "id",
+        "range_": "range",
+    }
+
+    def __init__(self, id_, range_=None, registration=None):
+        """
+        id_ : str
+        range_ : str (version range)
+        registration : str
+        """
+        self.id_ = id_
+        self.range_ = range_
+        self.registration = registration
+
+
+class RegistrationPageResponse(NuGetResponse):
+
+    json_key_map = {
+        "id_": "@id",
+    }
+
+    def __inti__(self, id_, count, lower, upper, items, parent):
+        """
+        id_ : string
+        count : int
+        lower : string
+        upper : string
+        items : array of MetadataIndexLeaf objects
+        parent : string
+        """
+        self.id_ = id_
+        self.count = count
+        self.lower = lower
+        self.upper = upper
+        self.items = items
+        self.parent = parent
+
+
+class RegistrationLeafResponse(NuGetResponse):
+
+    json_key_map = {
+        "id_": "@id",
+        "catalog_entry": "catalogEntry",
+        "pacakge_content": "pacakgeContent",
+    }
+
+    def __init__(self, id_, **kwargs):
+        """
+        id_ : str
+        catalog_entry : str
+        listed : bool
+        pacakge_content : str
+        published : str (ISO 8601 timestamp)
+        registration : str
+        """
+        self.id_ = id_
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
 
 class ContentResponse(NuGetResponse):
-    pass
+
+    def __init__(self, versions):
+        """
+        versions : array of str
+        """
+        self.versions = versions
 
 
-class CatalogResponse(NuGetResponse):
-    pass
+class CatalogIndexResponse(NuGetResponse):
+
+    json_key_map = {
+        "commit_id": "commitId",
+        "commit_timestamp": "commitTimeStamp",
+    }
+
+    def __init__(self, commit_id, commit_timestamp, count, items):
+        """
+        commit_id : str
+        commit_timestamp : str
+        count : int
+        itesm : list of CatalogIndexPage objects
+        """
+        self.commit_id = commit_id
+        self.commit_timestamp = commit_timestamp
+        self.count = count
+        self.items = items
+
+
+class CatalogIndexPage(NuGetResponse):
+
+    json_key_map = {
+        "id_": "@id",
+        "commit_id": "commitId",
+        "commit_timestamp": "commitTimeStamp",
+    }
+
+    def __init__(self, id_, commit_id, commit_timestamp, count):
+        """
+        id_ : str
+        commit_id : str
+        commit_timestamp : str
+        count : int
+        """
+        self.id_ = id_
+        self.commit_id = commit_id
+        self.commit_timestamp = commit_timestamp
+        self.count = count
+
+
+class CatalogPage(NuGetResponse):
+
+    json_key_map = {
+        "commit_id": "commitId",
+        "commit_timestamp": "commitTimeStamp",
+    }
+
+    def __init__(self, commit_id, commit_timestamp, count, items, parent):
+        """
+        commit_id : str
+        commit_timestamp : str
+        count : int
+        items : list of CatalogItem objects
+        parent : str
+        """
+        self.commit_id = commit_id
+        self.commit_timestamp = commit_timestamp
+        self.count = count
+        self.items = items
+        self.parent = parent
+
+
+class CatalogItem(NuGetResponse):
+
+    json_key_map = {
+        "id_": "@id",
+        "type_": "@type",
+        "commit_id": "commitId",
+        "commit_timestamp": "commitTimeStamp",
+        "nuget_id": "nuget:id",
+        "nuget_version": "nuget:version",
+    }
+
+    def __init__(self, id_, type_, commit_id, commit_timestamp,
+                 nuget_id, nuget_version):
+        """
+        id_ : str
+        type_ : str
+        commit_id : str
+        commit_timestamp : str
+        nuget_id : str
+        nuget_version : str
+        """
+        self.id_ = id_
+        self.type_ = type_
+        self.commit_id = commit_id
+        self.commit_timestamp = commit_timestamp
+        self.nuget_id = nuget_id
+        self.nuget_version = nuget_version
+
+
+class CatalogLeaf(NuGetResponse):
+
+    json_key_map = {
+        "type_": "@type",
+        "catalog_commit_id": "catalog:commitId",
+        "catalog_commit_timestamp": "catalog:commitTimeStamp",
+        "id_": "id",
+    }
+
+    def __init__(self, type_, catalog_commit_id, catalog_timestamp,
+                 id_, published, version):
+        """
+        type_ : str or array of str
+        catalog_commit_id : str
+        catalog_commit_timestamp : str
+        id_ : str
+        published : str
+        version : str
+        """
+        self.type_ = type_
+        self.catalog_commit_id = catalog_commit_id
+        self.catalog_timestamp = catalog_timestamp
+        self.id_ = id_
+        self.published = published
+        self.version = version
 
 
 def _rename_keys(obj):
