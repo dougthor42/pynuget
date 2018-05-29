3c1747b5206c83857c09356dc708e3198d3f1d0e
Douglas Thor <doug.thor@gmail.com>
2018-05-29 14:07:19 -0700

    Update __init__.py to 1st use default config, then use instance config

--- src/pynuget/__init__.py
+++ src/pynuget/__init__.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 """
 """
-import os
 import sys
+from pathlib import Path
 
 from flask import Flask
 
 from ._logging import setup_logging
 
-
-# TODO: switch to instance path, move config to look in /var/www
-#instance_path = "/var/www/pynuget/instance"
-instance_path = os.path.abspath(".")
-config_file = os.path.join(instance_path, "config.py")
-
 app = Flask(__name__)
-app.config.from_pyfile(config_file)
+app.config.from_object('pynuget.default_config')
+
+# Attempt to import the instance configuration.
+instance_path = Path("/var/www/pynuget/instance")
+config_file = instance_path / "config.py"
+
+if config_file.exists():
+    app.config.from_pyfile(str(config_file))
 
 logger = setup_logging(to_console=True,
                        to_file=False,
                        log_path=app.config['LOG_PATH'],
                        )
+
+msg = "Instance Config file `{}` found: {}"
+logger.info(msg.format(config_file, config_file.exists()))
 
 # contrary to Python style, the view imports must be *after* the application
 # object is created.
