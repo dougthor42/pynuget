2440867a6b2a6e8a72f5860ebe79d729d6020f04
Douglas Thor <doug.thor@gmail.com>
2018-05-29 14:52:20 -0700

    Only set up files during __init__.py if we're testing things

--- runserver.py
+++ runserver.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
 
 from pynuget import app
 
 PORT = 5000
+
+os.environ['PYNUGET_CONFIG_TYPE'] = 'TESTING'
 
 app.run(debug=True, port=PORT)
--- src/pynuget/__init__.py
+++ src/pynuget/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 """
+import os
 import sys
 from pathlib import Path
 
@@ -28,13 +29,14 @@ msg = "Instance Config file `{}` found: {}"
 logger.info(msg.format(config_file, config_file.exists()))
 
 # Now let's make sure that all our files are set up properly
-from pynuget.commands import init                              # noqa
-init(server_path=app.config['SERVER_PATH'],
-     package_dir=app.config['PACKAGE_DIR'],
-     db_name=app.config['DB_NAME'],
-     db_backend=app.config['DB_BACKEND'],
-     apache_config=app.config['APACHE_CONFIG'],
-     )
+if os.getenv("PYNUGET_CONFIG_TYPE", None) == 'TESTING':
+    from pynuget.commands import init                              # noqa
+    init(server_path=app.config['SERVER_PATH'],
+         package_dir=app.config['PACKAGE_DIR'],
+         db_name=app.config['DB_NAME'],
+         db_backend=app.config['DB_BACKEND'],
+         apache_config=app.config['APACHE_CONFIG'],
+         )
 
 # contrary to Python style, the view imports must be *after* the application
 # object is created.
