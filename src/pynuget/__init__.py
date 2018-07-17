08fad5a4ead9855987f8dcaa6d582e4ed7ad51ba
Douglas Thor <doug.thor@gmail.com>
2018-07-17 15:27:24 -0700

    Revert "Reorganize imports in __init__.py"
    This reverts commit 3385dc48f66178ca73f71a34cf81a19b9ab987d4.

--- src/pynuget/__init__.py
+++ src/pynuget/__init__.py
@@ -2,6 +2,6 @@
 """
 """
 from pynuget._logging import setup_logging
-from pynuget.app_factory import create_app
-
 logger = setup_logging(to_console=True)
+
+from pynuget.app_factory import create_app
