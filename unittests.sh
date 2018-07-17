7dcc68fbed777f0c7bb3432d69300ed7fdc17e59
Douglas Thor <doug.thor@gmail.com>
2018-07-17 14:56:30 -0700

    Forgot that I'm using a 'src' dir in unittests.sh...

--- unittests.sh
+++ unittests.sh
@@ -3,7 +3,7 @@ export PYNUGET_CONFIG_TYPE=TESTING
 # If we ran on Windows last, __pycache__ might be setup incorrect.
 # Just delete it to make things easier.
 rm -r tests/__pycache__
-rm -r pynuget/__pycache__
+rm -r src/pynuget/__pycache__
 
 pytest -ra --cov-report term-missing --cov pynuget tests/ --durations=5 "$@"
 
