a36e07931e75fb03e19c4bc65e5c3457255f9c85
Douglas Thor <doug.thor@gmail.com>
2018-07-17 08:07:04 -0700

    Add unittests.sh

---
+++ unittests.sh
@@ -0,0 +1,10 @@
+export PYNUGET_CONFIG_TYPE=TESTING
+
+# If we ran on Windows last, __pycache__ might be setup incorrect.
+# Just delete it to make things easier.
+rm -r tests/__pycache__
+rm -r pynuget/__pycache__
+
+pytest -ra --cov-report term-missing --cov pynuget tests/ --durations=5 "$@"
+
+unset PYNUGET_CONFIG_TYPE
