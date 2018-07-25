1dba199b85a5e8ade9b483bbcb55ae8606b98f05
Douglas Thor <doug.thor@gmail.com>
2018-07-25 13:43:23 -0700

    Optionally run coverage when running the local dev server

--- runserver.py
+++ runserver.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 from pathlib import Path
+
+run_cov = False
+if len(sys.argv) > 1 and sys.argv[1] == "--cov":
+    import coverage
+    cov = coverage.Coverage(branch=True, source=['src/pynuget'])
+    cov.start()
+    run_cov = True
 
 from pynuget import commands
 from pynuget import default_config
@@ -26,4 +34,9 @@ commands._create_directories(server_path, package_dir, log_dir)
 commands._create_db(db_backend, db_name, server_path)
 
 app = create_app()
-app.run(debug=True, port=PORT)
+app.run(debug=True, port=PORT, use_reloader=not run_cov)
+
+if run_cov:
+    cov.stop()
+    cov.save()
+    cov.html_report()
